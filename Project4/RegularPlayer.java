9ed5ad1c12116ab833bfe1498ee643ef83ed3dc7
nchen2211 <nchrysilla@gmail.com>
2015-08-30 12:14:23 -0700

    Java Project 4 - Updated Roulette - Regular Player class

---
+++ Project4/RegularPlayer.java
@@ -0,0 +1,23 @@
+
+public class RegularPlayer extends Player {
+	
+	public RegularPlayer(int initMoney)
+	{
+		super(initMoney);
+	}
+	
+	
+	public int getBalance()
+	{
+		return super.getBalance();
+	}
+	
+	
+	public void money()
+	{
+		super.money();
+	}
+	
+	
+
+}
