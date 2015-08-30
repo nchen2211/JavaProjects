380e8d12b9976d3775bc827d75cf5e964179ab53
nchen2211 <nchrysilla@gmail.com>
2015-08-30 12:26:58 -0700

    String manipulation

---
+++ Lab2/Names.java
@@ -0,0 +1,26 @@
+/*  Program:Names
+    Author:Noreen Chrysilla
+    Class:CSCI 145
+    Date:3/9/2014
+    Description:Added name using tab and add one column
+
+    I certify that the code below is modified from the Lab2 PDF.
+
+	Exception(s): N/A
+
+*/
+
+public class Names {
+	
+		public static void main (String[] args)
+		{
+			System.out.println ();
+			System.out.println (" \tName\t\tHometown\t\tMajor");
+			System.out.println ("\t====\t\t========\t\t=====");
+			System.out.println ("\tSally\t\tRoanoke\t\t\tComputer Science");
+			System.out.println ("\tAlexander\tWashington\t\tMath");
+			System.out.println ("\tNoreen\t\tGuang Zhou\t\tComputer Science");
+			System.out.println();
+			System.out.println ("Author: Noreen Chrysilla");
+		}
+}
