0b886a569bb08f439de474dff80a6a05518d76ac
nchen2211 <nchrysilla@gmail.com>
2015-08-30 12:28:43 -0700

    String student grades

---
+++ Lab2/StudentGrades.java
@@ -0,0 +1,28 @@
+/*  Program:Student Grades
+    Author:Noreen Chrysilla
+    Class:CSCI 145
+    Date:3/9/2014
+    Description:Created table for listing student grade
+
+    I certify that the code below is  my own work
+
+	Exception(s): N/A
+
+*/
+
+public class StudentGrades {
+	
+		public static void main(String[] args)
+		{
+			System.out.println (" \tName\t\tLab\t\tBonus\t\tTotal") ;
+			System.out.println ("\t----\t\t---\t\t-----\t\t-----");
+			System.out.println ("\tJoe\t\t43\t\t7\t\t"+ (43+7));
+			System.out.println ("\tWilliam\t\t50\t\t8\t\t" + (50+8));
+			System.out.println ("\tMary Sue\t39\t\t10\t\t" + (39+10));
+			System.out.println ("\tStephanie\t50\t\t10\t\t" + (50+10));
+			System.out.println ("\tChristopher\t49\t\t10\t\t" + (49+10));
+			System.out.println();
+			System.out.println ("Author: Noreen Chrysilla");
+		}
+
+}
